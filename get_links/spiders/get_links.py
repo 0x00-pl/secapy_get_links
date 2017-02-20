f02b6e4d9eeaf53b1a0940f945b1982e9f815360
pl <0x00.pl@gmail.com>
2017-02-20 15:10:00 +0800

    add protocol

--- get_links/spiders/get_links.py
+++ get_links/spiders/get_links.py
@@ -4,9 +4,9 @@ from urllib.parse import urlparse
 
 class MySpider(scrapy.Spider):
     name = "getall"
-    allowed_domains = ["4f61.com"]
+    allowed_domains = ["baidu.com"]
     start_urls = [
-        "http://www.4f61.com/",
+        "http://www.sina.com/",
     ]
 
     def parse(self, response):
@@ -17,19 +17,27 @@ class MySpider(scrapy.Spider):
         # iterate over links
         for link in all_links:
             abs_link = response.urljoin(link)
-            if urlparse(abs_link).netloc.endswith(self.allowed_domains[0]):
-                yield {"link": abs_link}
+            url_parsed = urlparse(abs_link)
+            if url_parsed.netloc.endswith(self.allowed_domains[0]):
+                yield {"link": abs_link, "netloc": url_parsed.netloc, "scheme": url_parsed.scheme}
 
 
 class MyPipeline(object):
-    results = []
+    results = set()
 
     def process_item(self, item, spider):
-        MyPipeline.results.append(dict(item))
+        self.results.add(dict(item)['netloc'])
 
 
-def main():
+def main(start_urls, allowed_domains=None):
     from scrapy.crawler import CrawlerProcess
+
+    if allowed_domains is None:
+        netlocs = [urlparse(i).netloc for i in start_urls]
+        netlocs = set(netlocs)
+        allowed_domains = list(netlocs)
+
+    MyPipeline.results = set()
 
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
@@ -37,11 +45,14 @@ def main():
         'LOG_ENABLED': False
     })
 
-    process.crawl(MySpider)
+    process.crawl(MySpider, start_urls=start_urls, allowed_domains=allowed_domains)
     process.start()
 
-    print(MyPipeline.results)
+    # [print(i)for i in MyPipeline.results]
+    # print(len(MyPipeline.results))
+    return list(MyPipeline.results)
 
 
 if __name__ == "__main__":
-    main()
+    res = main(["http://www.baidu.com"], ["baidu.com"])
+    [print(i) for i in res]
