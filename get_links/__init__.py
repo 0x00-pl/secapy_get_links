663ce8fe0159b4c32b531f90a4b6f0f947085ec6
pl <0x00.pl@gmail.com>
2017-02-24 17:16:49 +0800

    fix module

--- get_links/__init__.py
+++ get_links/__init__.py
@@ -2,4 +2,4 @@ from .spiders import get_links
 
 
 def simple_get(start_urls, allowed_domains=None):
-    get_links.main(start_urls, allowed_domains)
+    return get_links.main(start_urls, allowed_domains)
