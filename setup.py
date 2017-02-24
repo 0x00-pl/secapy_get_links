24ee14d44be38ba675775c5e29d8285a6cdab8da
pl <0x00.pl@gmail.com>
2017-02-24 17:03:22 +0800

    package file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+__author__ = 'pl'
+
+setup(
+    name="get_links",
+    version="0.1",
+    packages=['get_links'],
+    install_requires=['scrapy'],
+    zip_safe=True,
+)
