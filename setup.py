5b0ba23bfa4f6818190a73ae2fac11c492e1a2e5
pl <0x00.pl@gmail.com>
2017-03-06 14:24:40 +0800

    fix, add more packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = 'pl'
 setup(
     name="get_links",
     version="0.5",
-    packages=['get_links'],
+    packages=['get_links', 'get_links.spiders'],
     install_requires=['scrapy'],
     zip_safe=True,
 )
